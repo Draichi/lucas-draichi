3a4cef5b56dd2c9527fa74871e897f7604128d62
@Draichi <lucasdraichi@gmail.com>
2022-12-02 00:52:07 -0300

    wip: adding text

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,17 @@
 import "./style.css";
-import "./hero";
 import * as THREE from "three";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
-import meshToonTextureImage from "/toonTexture.jpg";
-//Variables for setup
 
-const container = document.querySelector(".scene.one") as HTMLDivElement;
+//Variables for setup
+const canvas = document.getElementById("webgl-renderer") as HTMLCanvasElement;
 
 //Create scene
 const scene = new THREE.Scene();
 
 const fov = 35;
-const aspect = container.clientWidth / container.clientHeight;
+const aspect = window.innerWidth / window.innerHeight;
 const near = 0.9;
 const far = 1000;
 
@@ -27,21 +25,25 @@ if (window.innerWidth <= 400) {
   gltfLoader.load(
     "/3d-models/low_poly_mobile_phone/scene.gltf",
     ({ scene: modelScene }) => {
-      modelScene.scale.set(2, 2, 2);
-      modelScene.rotation.x = 2;
-      modelScene.position.set(0.0, 7, 0.0);
+      modelScene.scale.set(3, 3, 3);
+      modelScene.rotation.x = -Math.PI * 1.25;
+      modelScene.position.set(0, 8, 0);
       scene.add(modelScene);
       camera.lookAt(modelScene.position);
+      camera.position.y = -9;
+      camera.position.x = -0.75;
     }
   );
 } else {
   gltfLoader.load(
     "/3d-models/laptop_computer_low_poly/scene.gltf",
     ({ scene: modelScene }) => {
+      modelScene.name = "laptop_computer_low_poly";
+      console.log(modelScene.name);
       modelScene.scale.set(2, 2, 2);
       modelScene.rotation.x = 2;
       modelScene.position.set(0.0, 2, 0.0);
-      scene.add(modelScene);
+      // scene.add(modelScene);
       camera.lookAt(modelScene.position);
     }
   );
@@ -51,103 +53,54 @@ if (window.innerWidth <= 400) {
 const renderer = new THREE.WebGLRenderer({
   antialias: true,
   alpha: true,
+  canvas,
 });
 
-renderer.setSize(container.clientWidth, container.clientHeight);
-renderer.setPixelRatio(window.devicePixelRatio);
-
-container.appendChild(renderer.domElement);
-
-const textureLoader = new THREE.TextureLoader();
-const meshToonTexture = textureLoader.load(meshToonTextureImage);
-
-var geometry = new THREE.BoxGeometry();
-var material = new THREE.MeshToonMaterial({
-  color: "#CEE1F3",
-  gradientMap: meshToonTexture,
-});
-var box = new THREE.Mesh(geometry, material);
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
 const light = new THREE.DirectionalLight("#fff", 1);
 light.position.set(0, -2, 0);
-box.scale.set(2, 2, 2);
-box.position.set(0.0, 2, 0.0);
 
-scene.add(light);
-animate();
+const mesh2 = new THREE.Mesh(
+  new THREE.ConeGeometry(1, 2, 32),
+  new THREE.MeshBasicMaterial()
+);
 
-function animate() {
-  renderer.render(scene, camera);
-  requestAnimationFrame(animate);
-}
+mesh2.position.set(0, 5, 0);
+camera.lookAt(mesh2.position);
+
+scene.add(light, mesh2);
 
 function onWindowResize() {
-  camera.aspect = container.clientWidth / container.clientHeight;
+  (camera.aspect = window.innerWidth), window.innerHeight;
   camera.updateProjectionMatrix();
-  renderer.setSize(container.clientWidth, container.clientHeight);
+  renderer.setSize(window.innerWidth, window.innerHeight);
 }
+
+window.addEventListener("mousemove", (event) => {
+  const { clientX } = event;
+  const { clientWidth } = canvas;
+
+  // camera.rotation.y = (clientX / clientWidth - 0.5) * -1;
+  const modelScene =
+    scene?.getObjectByName("laptop_computer_low_poly") ||
+    ({} as THREE.Object3D);
+  // modelScene.rotation.y = (clientX / clientWidth - 0.5) * -0.3;
+});
 
 window.addEventListener("resize", onWindowResize);
 
 gsap.registerPlugin(ScrollTrigger);
-
-camera.position.set(0, 0, 0);
 
 ScrollTrigger.defaults({
   immediateRender: false,
   scrub: true,
 });
 
-const iPhoneAnimation = gsap.timeline();
+function tick() {
+  renderer.render(scene, camera);
+  requestAnimationFrame(tick);
+}
 
-iPhoneAnimation.to(camera.position, {
-  y: -5,
-  scrollTrigger: {
-    trigger: ".section-three",
-
-    endTrigger: ".section-five",
-    end: "top bottom",
-  },
-});
-
-iPhoneAnimation.to(camera.rotation, {
-  y: 0.25,
-  scrollTrigger: {
-    trigger: ".section-four",
-
-    start: "top bottom",
-    end: "top top",
-  },
-});
-
-iPhoneAnimation.to(box.rotation, {
-  z: -0.25,
-  scrollTrigger: {
-    trigger: ".section-four",
-
-    start: "top bottom",
-    end: "top top",
-  },
-});
-
-iPhoneAnimation.to(box.position, {
-  x: -25,
-  y: 10,
-  scrollTrigger: {
-    trigger: ".section-five",
-
-    start: "top bottom",
-    end: "top top",
-  },
-});
-
-iPhoneAnimation.to(box.rotation, {
-  x: 25,
-  y: 10,
-  scrollTrigger: {
-    trigger: ".section-five",
-
-    start: "top bottom",
-    end: "top top",
-  },
-});
+tick();
