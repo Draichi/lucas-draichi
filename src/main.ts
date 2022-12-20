c81a6e977970bb4ca5d73c7472b7ac9c56b5c8fa
@Draichi <lucasdraichi@gmail.com>
2022-12-20 01:35:16 -0300

    feat: adds animations to above the fold section

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,116 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
 
-// gsap.to(camera.rotation, {
-//   y: Math.PI * 0.05,
-//   duration: 2,
-//   delay: 1,
-//   ease: "power1.easeOut",
-// });
+const aboveTheFoldTimeline = gsap.timeline({
+  defaults: {
+    duration: 0.85,
+    ease: "power3.out",
+  },
+});
+
+aboveTheFoldTimeline.fromTo(
+  ".above-the-fold__word-1",
+  {
+    x: "100%",
+    opacity: 0,
+  },
+  {
+    x: 0,
+    opacity: 1,
+  }
+);
+
+aboveTheFoldTimeline.fromTo(
+  ".above-the-fold__word-2",
+  {
+    y: "100%",
+    opacity: 0,
+  },
+  {
+    y: 0,
+    opacity: 1,
+  },
+  "<40%"
+);
+
+aboveTheFoldTimeline.fromTo(
+  ".above-the-fold__word-3",
+  {
+    x: "-100%",
+    opacity: 0,
+  },
+  {
+    x: 0,
+    opacity: 1,
+  },
+  "<40%"
+);
+
+aboveTheFoldTimeline.fromTo(
+  ".above-the-fold__word-4",
+  {
+    y: "-100%",
+    opacity: 0,
+  },
+  {
+    y: 0,
+    opacity: 1,
+  },
+  "<40%"
+);
+
+aboveTheFoldTimeline.fromTo(
+  ".above-the-fold__word-5",
+  {
+    x: "-100%",
+    opacity: 0,
+  },
+  {
+    x: 0,
+    opacity: 1,
+  },
+  "<40%"
+);
+
+aboveTheFoldTimeline.fromTo(
+  ".above-the-fold__word-6",
+  {
+    y: "100%",
+    opacity: 0,
+  },
+  {
+    y: 0,
+    opacity: 1,
+  },
+  "<40%"
+);
+
+aboveTheFoldTimeline.fromTo(
+  ".above-the-fold-buttons a",
+  {
+    y: "-100%",
+    opacity: 0,
+  },
+  {
+    y: 0,
+    opacity: 1,
+    stagger: 0.2,
+  },
+  "<80%"
+);
+
+aboveTheFoldTimeline.fromTo(
+  ".above-the-fold__title",
+  {
+    y: "-100%",
+    opacity: 0,
+  },
+  {
+    y: 0,
+    opacity: 1,
+  },
+  "<70%"
+);
 
 ScrollTrigger.defaults({
   immediateRender: false,
